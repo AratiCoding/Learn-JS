00f5879f65f5419c9850853b258ffa1dd6b0e61d
Arati Chanda <114807223+AratiCoding@users.noreply.github.com>
2025-02-25 12:21:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";  //treat all code of JS as newer version 
+
+let name="Arati"
+let age=24
+let isLogedIn=true
+
+//Datatypes in JS
+//  string
+//  number
+//   bigint
+//   boolean
+//   null    (remember null is a object)
+//  undefined  
+//   symbol (unique)
+//   object   
+//  typeOf
