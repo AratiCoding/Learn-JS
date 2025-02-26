8f15f391cae9a1dfdccd67752cad48a4fafb01e7
Arati Chanda <114807223+AratiCoding@users.noreply.github.com>
2025-02-26 12:57:42 +0000

    dates in JS

---
+++ 01_basics/06_dates.js
@@ -0,0 +1,16 @@
+let toDay=new Date()
+// console.log(toDay)
+// console.log(toDay.toString());
+// console.log(toDay.toDateString());
+// console.log(typeof toDay)
+
+let myDate=new Date(2025,7,25)    //in JS month starts with 0
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+let myTimeStamp=Date.now()
+// console.log(myDate.getTime())
+// console.log(Date.now())
+
+let newdate=new Date()
+console.log(newdate)
